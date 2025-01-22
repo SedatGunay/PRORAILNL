a2ddea6454005d87fb007734e5995817709e9fe3
Boenka <70281105+Boenka@users.noreply.github.com>
2025-01-22 20:18:25 +0100

    Update baseline2.py

--- baseline2.py
+++ baseline2.py
@@ -132,8 +132,8 @@ class RailNetwork:
 
 
 rail_network = RailNetwork()
-rail_network.load_stations("StationsHolland.csv") 
-rail_network.load_connections("ConnectiesHolland.csv") 
+rail_network.load_stations(r"C:\Users\koste\Documents\GitHub\PRORAILNL\data\NL\StationsNationaal.csv") 
+rail_network.load_connections(r"C:\Users\koste\Documents\GitHub\PRORAILNL\data\NL\ConnectiesNationaal.csv") 
 
 time_limit = 180 
 num_trajectories = 100000 
